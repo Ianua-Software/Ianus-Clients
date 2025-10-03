6f87685d75ee00e5ef168b7531e7ac7c606a453d
Florian Kroenert <Florian.Kroenert@mailbox.org>
2025-10-03 17:06:05 +0200

    Switch to release builds to fix Solution Checker warnings

---
+++ frontend/react/dataverse/pcf/canvas/fluentui8/webpack.config.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  mode: "production",         // ✅ Ensures no eval-based devtool
+  devtool: "source-map",      // ✅ Optional, better for production debugging
+};
